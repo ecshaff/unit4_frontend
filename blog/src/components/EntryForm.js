ae1119a22ea3b326c4e1db9b4379636891a7e4a9
Lauren Mellon <laumellon@gmail.com>
2019-06-07 22:18:12 -0400

    added some CSS to identify components

--- blog/src/components/EntryForm.js
+++ blog/src/components/EntryForm.js
@@ -10,13 +10,31 @@ import React , { Component } from 'react';
 class EntryForm extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            date: '',
+            title: '',
+            text: '',
+            image: '',
+            author: '',
+            status: 'new'
+        }
     }
     render() {
         return (
             <form>
                 <fieldset>
                     <legend>Create New Entry</legend>
+                    <label htmlFor='date'>Date</label>
+                    <input type='date' id='date'/>
+                    <label htmlFor='author'>Author</label>
+                    <input type='text' id='author'/>
+                    <label htmlFor='title'>Title</label>
+                    <input type='text' id='title'/>
+                    <label htmlFor='text'>Text</label>
+                    <textarea id='text'></textarea>
+                    <label htmlFor='image'>Image URL</label>
+                    <input type='text' id='image'/>
+                    <input type='submit' value='Post Entry'/>
                 </fieldset>
             </form>
         );
